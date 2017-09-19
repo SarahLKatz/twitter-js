7972b0581054df2e3be4257c0007dcaec8a51768
Jonathan Martinez <jonm90@gmail.com>
2017-09-19 11:01:12 -0400

    added to router file

--- app.js
+++ app.js
@@ -1,36 +1,23 @@
 const express = require('express');
 const app = express();
 const nunjucks =  require('nunjucks')
+const routes = require('./routes');
 
 const PORT = 3000;
-const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 
-app.use(function(req,res,next){
+nunjucks.configure('views', {noCache: true});
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views'); // point nunjucks to the proper directory for templates
+
+app.use(function(req, res, next){
   console.log(req.method, req.url);
   next();
-});
-
-app.get('/', function(req, res){
-  res.render( 'index', {title: 'Hall of Fame', people: people} );
 });
 
 app.listen(PORT, function(){
   console.log('server listening');
 })
 
-var locals = {
-  title: 'An Example',
-  people: [
-      { name: 'Gandalf'},
-      { name: 'Frodo' },
-      { name: 'Hermione'}
-  ]
-};
-nunjucks.configure('views', {noCache: true});
-nunjucks.render('index.html', locals, function (err, output) {
-  console.log(output);
-});
-
-app.set('view engine', 'html'); // have res.render work with html files
-app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
-nunjucks.configure('views'); // point nunjucks to the proper directory for templates
+app.use('/', routes);
